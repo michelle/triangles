d90505cfb7fd76db46191103dbf0e6a0aaa1005f
Michelle Bu <michelle@michellebu.com>
2013-03-16 19:15:21 -0700

    done

--- da.js
+++ da.js
@@ -1,33 +1,36 @@
-/** Constants */
-BLUECHANCE = .05;
-ROWS = 8;
-COLUMNS = 20;
-GREYMAXCHANCE = 85;
-GREYMINCHANCE = 30;
-
-
-/** Populates page with triangles based on the following: 
+/** Populates page with triangles based on the following:
   *  Chance of a blue = 5%.
   *  Chance of a grey is between GREYMAXCHANCE and GREYMINCHANCE depending on 
   *    column.
   */
-function populate(div) {
-  for (var i = 0; i < COLUMNS; i += 1) {
-    div.append(createColumn(i));
+function Triangles(div, width, height) {
+  this._GREYMAXCHANCE = 85;
+  this._GREYMINCHANCE = 30;
+  this._BLUECHANCE = .06;
+  this._WHITECHANCE = .01;
+
+  this._div = div;
+
+  this._columns = Math.floor(width / 42);
+  this._rows = Math.ceil(height / 42);
+  for (var i = 0; i < this._columns; i += 1) {
+    this._createColumn();
   }
-}
+};
 
 /** Creates column N. */
-function createColumn(n) {
-  var greyChance = genRand(GREYMINCHANCE, GREYMAXCHANCE) / 100;
-  var col = $('<div></div>').addClass('col').attr('id', n);
+Triangles.prototype._createColumn = function() {
+  var greyChance = genRand(this._GREYMINCHANCE, this._GREYMAXCHANCE) / 100;
+  var col = $('<div></div>').addClass('col');
 
-  for (var i = 0; i < ROWS; i += 1) {
+  for (var i = 0; i < this._rows; i += 1) {
     var chance = Math.random();
     var el = $('<div></div>');
     if (chance < greyChance) {
       el.addClass('triangle');
-      if (chance < BLUECHANCE) {
+      if (chance < this._WHITECHANCE) {
+        el.addClass('white');
+      } else if (chance < this._BLUECHANCE) {
         el.addClass('blue');
       }
     } else {
@@ -36,40 +39,65 @@ function createColumn(n) {
     col.append(el);
 
   }
-  return col;
-}
+  this._div.append(col);
+};
 
-/** Hides column N. */
-function hideColumn(n) {
-  if (n >= COLUMNS) {
-    return;
+Triangles.prototype.hide = function() {
+  var activeRow = $(this._div.children(':first'));
+
+  function hideNext() {
+    if (!activeRow) {
+      return;
+    }
+    row = activeRow;
+    nextRow = activeRow.next();
+    activeRow = nextRow.length > 0 ? $(nextRow[0]) : null;
+    row.animate({ opacity: 0 }, 100, hideNext);
   }
 
-  var colName = '#' + n;
-  $(colName).stop().animate({ 'opacity': '0' }, 100,
-      function() { hideColumn(n + 1); });
-}
+  hideNext();
+};
 
-/** Shows column N. */
-function showColumn(n) {
-  if (n >= COLUMNS) {
-    return;
+Triangles.prototype.show = function() {
+  var activeRow = $(this._div.children(':first'));
+
+  function showNext() {
+    if (!activeRow) {
+      return;
+    }
+    row = activeRow;
+    nextRow = activeRow.next();
+    activeRow = nextRow.length > 0 ? $(nextRow[0]) : null;
+    row.animate({ opacity: 1 }, 100, showNext);
   }
 
-  var colName = '#' + n;
-  $(colName).stop().animate({ 'opacity': '1' }, 100,
-      function() { showColumn(n + 1); });
-
-}
+  showNext();
+};
 
 /** Generates a random number between MIN and MAX */
 function genRand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 
 $(document).ready(function(){
   var div = $('#triangles');
-  populate(div);;
-  
+  var triangles;
+
+  generate = function() {
+    div.empty();
+    triangles = new Triangles(div, $(window).width(), $(window).height());
+  }
+  generate();
+
+  $(window).resize(generate);
+  $('#sidebar').hover(function() {
+    if (triangles) {
+      triangles.hide();
+    }
+  }, function() {
+    if (triangles) {
+      triangles.show();
+    }
+  });
 });
